9441152b4a0c2d6fd351e4a033452dd69b901dbb
zaxi <zaxi@zaxideMacBook-Pro.local>
2022-07-15 17:14:32 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ npm run build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-echo 'coderhdy.com' > CNAME
+# echo 'coderhdy.com' > CNAME
 
 git init
 git add -A
 git commit -m 'Deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:coderHdy/coderHdy.github.io.git master
+git push -f git@github.com:zaxihero/zaxihero.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:coderHDY/myBlog.git master:gh-pages
+# git push -f git@github.com:coderHDY/myBlog.git master:gh-pages
 
 cd -
